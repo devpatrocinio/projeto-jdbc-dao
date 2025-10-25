8ea1d4f79e71dcb188c3175c32a5ce8dba910559
Isaac Patrocinio <isaacpatrocinio27@gmail.com>
2025-10-25 12:27:09 -0300

    Criação da classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,17 @@
 package application;
 
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
+
 public class Program {
 
     public static void main(String[] args) {
+        Department d = new Department(10, "TI");
+        Seller s = new Seller(25, "João", "joao@gmail.com", LocalDate.now(), 3000.0, d);
 
+        System.out.println(d);
+        System.out.println(s);
     }
 }
