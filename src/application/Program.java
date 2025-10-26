107f36ad52de339c4779d51227357f259cccfab7
Isaac Patrocinio <isaacpatrocinio27@gmail.com>
2025-10-26 14:57:12 -0300

    Implementação do método findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,20 +2,14 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.time.LocalDate;
 
 public class Program {
 
     public static void main(String[] args) {
-        Department d = new Department(10, "TI");
-        Seller s = new Seller(25, "João", "joao@gmail.com", LocalDate.now(), 3000.0, d);
-
-        System.out.println(d);
-        System.out.println(s);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller sellerTest = sellerDao.findById(1);
+        System.out.println(sellerTest);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,25 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DBException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection connection;
+
+    public SellerDaoJDBC(Connection conn) {
+        this.connection = conn;
+    }
 
     @Override
     public void insert(Seller obj) {
@@ -24,7 +38,36 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String query = "SELECT seller.*, department.Name as DepName" +
+                " FROM seller" +
+                " INNER JOIN department" +
+                " ON DepartmentId = department.id" +
+                " WHERE seller.id = ?";
+
+        try {
+            stmt = connection.prepareStatement(query);
+            stmt.setInt(1, id);
+
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+
+                return new Seller(rs.getInt("Id"), rs.getString("Name"), rs.getString("Email"), rs.getDate("BirthDate").toLocalDate(), rs.getDouble("BaseSalary"), dep);
+            }
+            return null;
+
+        } catch (SQLException e) {
+            throw new DBException(e.getMessage());
+        } finally {
+            DB.closeStatement(stmt);
+            DB.closeResultSet(rs);
+        }
     }
 
     @Override
